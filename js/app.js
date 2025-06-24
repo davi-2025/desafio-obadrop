0cf364ffa4ddfa5cfa52dab84c7dcc74aa750eb2
Bruno Serrão Lins <2019003172@ifam.edu.br>
2025-06-24 17:19:08 -0400

    Criei Const de cada funcao

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const postsList = document.getElementById('posts-list');
+const newPostBtn = document.getElementById('new-post-btn');
+const formContainer = document.getElementById('form-container');
+const form = document.getElementById('post-form');
+const formTitle = document.getElementById('form-title');
+const postIdInput = document.getElementById('post-id');
+const titleInput = document.getElementById('title');
+const bodyInput = document.getElementById('body');
+const cancelBtn = document.getElementById('cancel-btn');
+
